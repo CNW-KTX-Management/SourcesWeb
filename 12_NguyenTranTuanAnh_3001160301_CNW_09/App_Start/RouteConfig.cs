4bfb503f3c406ba67ed2557863662b2c893d1802
Nguyễn Trần Tuấn Anh <nguyentrantuananh.97@gmail.com>
2020-12-09 19:35:27 +0700

    2

--- 12_NguyenTranTuanAnh_3001160301_CNW_09/App_Start/RouteConfig.cs
+++ 12_NguyenTranTuanAnh_3001160301_CNW_09/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _12_NguyenTranTuanAnh_3001160301_CNW_09
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Create", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
